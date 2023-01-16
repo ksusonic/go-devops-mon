640a86bbba28a2acf6fc30d62bc029e331326454
Daniil <ksusonic@yandex.ru>
2023-01-16 18:00:32 +0300

    inc7

--- internal/agent/config.go
+++ internal/agent/config.go
@@ -1,23 +1,31 @@
 package agent
 
 import (
+	"flag"
 	"log"
 
 	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	Address        string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+	Address        string `env:"ADDRESS"`
 	AddressScheme  string `env:"ADDRESS_SCHEME" envDefault:"http"`
-	ReportInterval string `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   string `env:"POLL_INTERVAL" envDefault:"2s"`
+	ReportInterval string `env:"REPORT_INTERVAL"`
+	PollInterval   string `env:"POLL_INTERVAL"`
 }
 
 func NewConfig() *Config {
 	var cfg Config
+
+	flag.StringVar(&cfg.Address, "a", "127.0.0.1:8080", "address of server with port if needed")
+	flag.StringVar(&cfg.ReportInterval, "r", "10s", "report interval")
+	flag.StringVar(&cfg.PollInterval, "p", "2s", "collect metrics interval")
+	flag.Parse()
+
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return &cfg
 }
--- internal/server/config.go
+++ internal/server/config.go
@@ -1,23 +1,32 @@
 package server
 
 import (
+	"flag"
 	"log"
 
 	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	Address          string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
-	FileDropInterval string `env:"STORE_INTERVAL" envDefault:"300s"`
-	StoreFile        string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	RestoreFile      bool   `env:"RESTORE" envDefault:"true"`
+	Address          string `env:"ADDRESS"`
+	FileDropInterval string `env:"STORE_INTERVAL"`
+	StoreFile        string `env:"STORE_FILE"`
+	RestoreFile      bool   `env:"RESTORE"`
 }
 
 func NewConfig() *Config {
 	var cfg Config
+
+	flag.StringVar(&cfg.Address, "a", "127.0.0.1:8080", "address of server with port if needed")
+	flag.BoolVar(&cfg.RestoreFile, "r", true, "bool if restore from file needed")
+	flag.StringVar(&cfg.FileDropInterval, "i", "300s", "interval to save metrics to file")
+	flag.StringVar(&cfg.StoreFile, "f", "/tmp/devops-metrics-db.json", "path to file for metrics save")
+	flag.Parse()
+
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return &cfg
 }
